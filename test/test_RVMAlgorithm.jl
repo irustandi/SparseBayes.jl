143f822f67f94f21a490af19ba67e9c78f9873de
Indrayana Rustandi <i_rustandi@yahoo.com>
2015-12-31 13:02:42 -0800

    wrap tests in a module

--- test/test_RVMAlgorithm.jl
+++ test/test_RVMAlgorithm.jl
@@ -1,3 +1,8 @@
+module TestProcessRVMAction
+
+using SparseBayes
+using Base.Test
+
 alpha = 1.
 s = 0.1
 factor = 0.01
@@ -6,3 +11,5 @@ addValue = (s * s) / factor
 @test isinf(SparseBayes.processRVMAction(SparseBayes.Delete, alpha, s, factor))
 @test_approx_eq SparseBayes.processRVMAction(SparseBayes.Hold, alpha, s, factor) alpha
 @test_approx_eq SparseBayes.processRVMAction(SparseBayes.Add, alpha, s, factor) addValue
+
+end
